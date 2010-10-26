3762b1616f24f36f30d104c168dbe0e76d9b395f
Luke Daley <ld@ldaley.com>
2010-10-27 01:59:36 +1000

    Tests for GRAILS-6855.

--- src/test/resources/projects/integration/test/integration/IntegrationTestTests.groovy
+++ src/test/resources/projects/integration/test/integration/IntegrationTestTests.groovy
@@ -1,9 +1,12 @@
 import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.codehaus.groovy.grails.plugins.PluginManagerHolder
 
 class IntegrationTestTests extends GroovyTestCase {
 
 	def bootstrapFlagService
 	def grailsApplication
+	
+	def pluginManager
 	
 	void testBootstrapWasCalled() {
 		assert bootstrapFlagService.setAtBootstrap == true
@@ -19,5 +22,8 @@ class IntegrationTestTests extends GroovyTestCase {
 		// Context needs to be configured to use the right path as it's root
 		assert viaHolder.getResourceAsStream("/css/main.css") != null
 	}
-		
+	
+	void testPluginManagerBeanIsTheSameAsInHolder() {
+		assert pluginManager.is(PluginManagerHolder.pluginManager)
+	}
 }
